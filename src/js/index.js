7ab5ce43b9957f3932ddd8515d4a16145507f2a7
Gustavofb <gusttavo.fernandesb@gmail.com>
2025-01-26 21:30:36 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,16 @@ const btnAvancar = document.getElementById("btn-avancar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function(){
+  const cartaVirada = cartao.querySelector(".carta-virada")
+  cartao.classList.toggle("virar");
+  cartaVirada.classList.toggle("mostra-fundo-carta");
+  const descricao = cartao.querySelector(".descricao");
+  descricao.classList.toggle("esconder");
+  });
+});
+
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
 
